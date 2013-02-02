91f39e3d4323150e76bc8e8aac9c3c22a6addb2c
David Cook <divergentdave@gmail.com>
2013-02-02 12:09:03 -0600

    Clean up Closure zip file when done

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 if [ ! -d "compiler-latest" ]; then
   curl -O "http://closure-compiler.googlecode.com/files/compiler-latest.zip"
-  unzip compiler-latest.zip -d compiler-latest
+  unzip compiler-latest.zip -d compiler-latest && rm compiler-latest.zip
 fi
 java -jar compiler-latest/compiler.jar --js memory-hole.js --js_output_file memory-hole.min.js
 
