ffafe2c76c557835abcf07cefbd2af29c0b2fdd1
David Cook <divergentdave@gmail.com>
2013-02-02 09:57:24 -0600

    Automatically put minified bookmarklet in README

--- build.sh
+++ build.sh
@@ -4,3 +4,10 @@ if [ ! -d "compiler-latest" ]; then
   unzip compiler-latest.zip -d compiler-latest
 fi
 java -jar compiler-latest/compiler.jar --js memory-hole.js --js_output_file memory-hole.min.js
+
+cat << 'END' > README.md
+Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+END
+echo -n 'Hello world. <a href="javascript:' >> README.md
+python -c "import urllib; open('README.md', 'a').write(urllib.quote(open('memory-hole.min.js', 'r').read()))"
+echo '">Bookmarklet</a> Etcetera.' >> README.md
